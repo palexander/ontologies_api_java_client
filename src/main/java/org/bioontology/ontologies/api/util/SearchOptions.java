e8f53f297ef628fbed42c8f7fa40ac4adf17ae66
mmartinez <marcosmartinez@udc.es>
2014-09-22 17:44:12 -0700

    Add constructor that accepts several search options

--- src/main/java/org/bioontology/ontologies/api/util/SearchOptions.java
+++ src/main/java/org/bioontology/ontologies/api/util/SearchOptions.java
@@ -1,25 +1,63 @@
 package org.bioontology.ontologies.api.util;
 
 /**
- *
+ * 
  * @author palexand
  */
 public class SearchOptions {
 
-    public boolean exactMatch;
-    public int pageSize;
-    public int pageNum;
+	public boolean exactMatch;
+	public String ontologies;	
+	public boolean requireDefinition;
+	public boolean includeProperties;
+	public boolean includeObsolete;
+	public String cui;
+	public String semanticTypes;
+	// Available attributes to include: prefLabel,synonym,definition,notation,cui,semanticType,properties
+	public String include;
+	public int pageNum;
+	public int pageSize;
+	
+	public SearchOptions() {
+		pageNum = 1;
+		pageSize = 50;		
+		exactMatch = false;
+	}
 
-    public SearchOptions() {
-        pageSize = 50;
-        pageNum = 1;
-        exactMatch = false;
-    }
+	public SearchOptions(int pageNum,int pageSize,  
+			boolean exactMatch, String ontologies, boolean requireDefinition,
+			boolean includeProperties, boolean includeObsolete, String cui,
+			String semanticTypes, String include) {		
+		this.pageNum = pageNum;
+		this.pageSize = pageSize;		
+		this.exactMatch = exactMatch;
+		this.ontologies = ontologies;		
+		this.requireDefinition = requireDefinition;
+		this.includeProperties = includeProperties;
+		this.includeObsolete = includeObsolete;
+		this.cui = cui;
+		this.semanticTypes = semanticTypes;
+		this.include = include;
+	}
 
-    public String toParams(String text) {
-        return "q=" + text
-                + "&pagesize=" + pageSize
-                + "&page=" + pageNum
-                + "&exact_match=" + exactMatch;
-    }
+	public String toParams(String text) {
+		String query = "q=" + text + "&pagesize=" + pageSize + "&page="
+				+ pageNum + "&exact_match=" + exactMatch;
+		if (ontologies!=null)
+			query+="&ontologies="+ontologies;
+		if (requireDefinition)
+			query+="&require_definition=" + requireDefinition;
+		if (includeProperties)
+			query+="&include_properties=" + includeProperties;
+		if (includeObsolete)
+			query+="&include_obsolete=" + includeObsolete;
+		if (cui!=null)
+			query+="&cui="+cui;
+		if (semanticTypes!=null)
+			query+="&semantic_types=" + semanticTypes;
+		if (include!=null)
+			query+="&include=" + include;
+
+		return query;
+	}
 }
